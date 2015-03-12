9936cdd8734b5db2122d4aee2509d6e345d120ad
OSBSS <akram@osbss.com>
2015-03-12 15:49:28 -0500

    Cleanup, DST, reduce delays
    - Accounted for DST in code (using DS3234lib3 library)
    - Cleanup code, rearrange all variables, properly labeled comments
    - Reduced and changed delays

--- GenericDatalogger_ADS1115.ino
+++ GenericDatalogger_ADS1115.ino
@@ -1,8 +1,11 @@
-// 12/9/2014 - Generic 4-channel Voltage Datalogger 16-bit v0.02
+// 3/12/2014 - Generic 4-channel Voltage Datalogger 16-bit v0.02
 
-// Maximum voltage reading with v0.02 of the device is upto 3.3V only, which is the voltage between VCC and GND of ADS1115.
-// Do NOT supply greater than 3.3V to the channels or it may damage the ADC.
-// Use a 5V Pro Mini to get more range or create a voltage divider circuit.
+// If you're using a 3.3V Pro Mini, the maximum voltage reading with v0.02 of the logger is upto 3.3V only,
+// which is the voltage between VCC and GND of ADS1115.Do NOT supply greater than 3.3V to the channels
+// or it may damage the ADC.
+
+// Use a 5V Pro Mini to get more range or create a voltage divider circuit
+// microSD card still needs 3.3V - use a voltage regulator
 
 
 #include <EEPROM.h>
@@ -12,55 +15,53 @@
 #include <DS3234lib3.h>
 #include <SdFat.h>
 
-PowerSaver chip;    // declare object for PowerSaver class
-Adafruit_ADS1115 ads;    // declare object for Adafruit_ADS1115 class
+// Launch Variables   ******************************
+long interval = 60;  // set logging interval in SECONDS, eg: set 300 seconds for an interval of 5 mins
+int dayStart = 12, hourStart = 14, minStart = 30;    // define logger start time: day of the month, hour, minute
+char filename[15] = "log.csv";    // Set filename Format: "12345678.123". Cannot be more than 8 characters in length, contain spaces or begin with a number
 
-// Main code stuff   ******************************
+// Global objects and variables   ******************************
 #define POWA 14    // pin 14 (A0) supplies power to microSD card breakout and 16-bit ADC
-int SDcsPin = 9;
-long interval = 10;  // interval in seconds (value automatically assigned by the GUI)
+#define LED 4  // pin 4 controls LED
+int SDcsPin = 9; // pin 9 is CS pin for MicroSD breakout
+int SHT_clockPin = 3;  // pin used for SCK on SHT15 breakout
+int SHT_dataPin = 5;  // pin used for DATA on SHT15 breakout
 
-// RTC stuff   ******************************
+PowerSaver chip;  	// declare object for PowerSaver class
 DS3234 RTC;    // declare object for DS3234 class
-int dayStart = 0, hourStart = 0, minStart = 0;    // start time: day of the month, hour, minute
-                                                  // values automatically assigned by the GUI
- 
-// SD card stuff   ******************************
-#define LED 4  // pin 7 controls LED
-SdFat sd;
-SdFile file;
-char filename[] = "log.csv";    // file name should be of the format "12345678.123". Cannot be more than 8 characters in length
- 
-// Stuff stuff ****************************************************************
-ISR(PCINT0_vect)  // setup interrupts on digital pin 8
+SHT15 sensor(SHT_clockPin, SHT_dataPin);  // declare object for SHT15 class
+SdFat sd; 		// declare object for SdFat class
+SdFile file;		// declare object for SdFile class
+
+// ISR ****************************************************************
+ISR(PCINT0_vect)  // Interrupt Vector Routine to be executed when pin 8 receives an interrupt.
 {
-  PORTB ^= (1<<PORTB1);
+  //PORTB ^= (1<<PORTB1);
+  asm("nop");
 }
 
 // setup ****************************************************************
 void setup()
 {
-  Serial.begin(19200);
+  Serial.begin(19200); // open serial at 19200 bps
+  
   pinMode(POWA, OUTPUT);
-  digitalWrite(POWA, HIGH);    // turn on SD card and ADC
   pinMode(LED, OUTPUT);
   
+  digitalWrite(POWA, HIGH);    // turn on SD card and ADC
+  delay(1);    // give some delay to ensure RTC and SD are initialized properly
+
   ads.begin();
   //ads.setGain(GAIN_TWO);  // set gain here
   
-  RTC.fetchAndSetTime();  // syncs date and time with the PC's clock
-  RTC.getLaunchParameters(interval, dayStart, hourStart, minStart); // get parameters from GUI
-  readFileName();  // read filename from EEPROM (assigned by GUI)
-  delay(500);    // give some delay to ensure the RTC gets proper date/time
-  
-  if(!sd.init(SPI_FULL_SPEED, SDcsPin))  // initialize SD card on the SPI bus
+  if(!sd.init(SPI_FULL_SPEED, SDcsPin))  // initialize SD card on the SPI bus  - very important
   {
-    delay(100);
+    delay(10);
     SDcardError();
   }
   else
   {
-    delay(50);
+    delay(10);
     file.open(filename, O_CREAT | O_APPEND | O_WRITE);  // open file in write mode and append data to the end of file
     delay(1);
     String time = RTC.timeStamp();    // get date and time from RTC
@@ -73,55 +74,73 @@ void setup()
     digitalWrite(LED, HIGH);
     delay(10);
     digitalWrite(LED, LOW);
-    delay(10);
   }
   
   RTC.checkInterval(hourStart, minStart, interval); // Check if the logging interval is in secs, mins or hours
   RTC.alarm2set(dayStart, hourStart, minStart);  // Configure begin time
   RTC.alarmFlagClear();  // clear alarm flag
-  chip.sleepInterruptSetup();    // setup sleep function on the ATmega328p. Power-down mode is used here
+  
+  chip.sleepInterruptSetup();    // setup sleep function & pin change interrupts on the ATmega328p. Power-down mode is used here
 }
 
 // loop ****************************************************************
 void loop()
 {
   digitalWrite(POWA, LOW);  // turn off microSD card to save power
-  delay(5);  // give some delay for SD card power to be low before processor sleeps to avoid it being stuck
+  delay(1);  // give some delay for SD card power to be low before processor sleeps to avoid it being stuck
   
-  sleeptime();
+  chip.turnOffADC();    // turn off ADC to save power
+  pinMode(A0, OUTPUT); // this is set for this particular version of the datalogger only
+                       // because the POWER pin is A0 and it is configured as INPUT after
+                       // turning ADC off which results in some power draw by the microSD card during sleep mode
+  
+  chip.turnOffSPI();    // turn off SPI bus to save power
+  //chip.turnOffWDT();  // turn off WatchDog Timer to save power (does not work for Pro Mini - only works for Uno)
+  chip.turnOffBOD();    // turn off Brown-out detection to save power
+  
+  chip.goodNight();    // put processor in extreme power down mode - GOODNIGHT!
+                       // this function saves previous states of analog pins and sets them to LOW INPUTS
+                       // average current draw on Mini Pro should now be around 0.195 mA (with both onboard LEDs taken out)
+                       // Processor will only wake up with an interrupt generated from the RTC, which occurs every logging interval
+  
   
   chip.turnOnADC();    // enable ADC after processor wakes up
   chip.turnOnSPI();   // turn on SPI bus once the processor wakes up
   delay(1);    // important delay to ensure SPI bus is properly activated
+  
   RTC.alarmFlagClear();    // clear alarm flag
   pinMode(POWA, OUTPUT);
   digitalWrite(POWA, HIGH);  // turn on SD card power
-  delay(50);    // give delay to let the SD card get full powa
+  delay(1);    // give delay to let the SD card get full powa
+  
+  RTC.checkDST(); // check and account for Daylight Savings Time in US
+  
+  // get ADC values from all channels
+  float adc0 = samples(0);
+  float adc1 = samples(1);
+  float adc2 = samples(2);
+  float adc3 = samples(3);
+    
+  // convert adc values to voltage
+  float V0 = voltage(adc0, 0);  // set gain here based on setting above. Options: 0(default), 1, 2, 4, 8, 16
+  float V1 = voltage(adc1, 0);
+  float V2 = voltage(adc2, 0);
+  float V3 = voltage(adc3, 0);
+  
   pinMode(SDcsPin, OUTPUT);
   if(!sd.init(SPI_FULL_SPEED, SDcsPin))    // very important - reinitialize SD card on the SPI bus
   {
-    delay(100);
+    delay(10);
     SDcardError();
   }
   else
   {
-    delay(50);
+    delay(10);
     file.open(filename, O_WRITE | O_AT_END);  // open file in write mode
     delay(1);
+    
     String time = RTC.timeStamp();    // get date and time from RTC
-    SPCR = 0;
-    
-    // get ADC values
-    float adc0 = samples(0);
-    float adc1 = samples(1);
-    float adc2 = samples(2);
-    float adc3 = samples(3);
-    
-    // get voltage
-    float V0 = voltage(adc0, 0);  // set gain here based on setting above. Options: 0(default), 1, 2, 4, 8, 16
-    float V1 = voltage(adc1, 0);
-    float V2 = voltage(adc2, 0);
-    float V3 = voltage(adc3, 0);
+    SPCR = 0;  // reset SPI control register
     
     file.print(time);
     file.print(",");
@@ -147,9 +166,7 @@ void loop()
     digitalWrite(LED, HIGH);
     delay(10);
     digitalWrite(LED, LOW);
-    delay(10);  
   }
-  
   RTC.setNextAlarm();      //set next alarm before sleeping
   delay(1);
 }
@@ -228,20 +245,3 @@ void SDcardError()
       delay(150);
     }
 }
- 
-//****************************************************************
-
-void sleeptime()
-{
-    chip.turnOffADC();
-    pinMode(A0, OUTPUT); // this is set for this particular version of the datalogger only
-                         // because the POWER pin is A0 and it is configured as INPUT after
-                         // turning ADC off which results in some power draw by the microSD card during sleep mode
-    chip.turnOffWDT();
-    chip.turnOffSPI();
-    chip.turnOffBOD();
-    chip.goodNight();
-}
-
-//****************************************************************
-
